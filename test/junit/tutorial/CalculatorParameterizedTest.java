46739964fcf4a5a016fbe6b25c39e229ec14b5c8
iwao harada <harada_iwao@ogis-ri.co.jp>
2012-06-28 17:00:18 +0900

    JUnit4.8とJUnit4.10が混在していたので、4.8をパスから削除

--- test/junit/tutorial/CalculatorParameterizedTest.java
+++ test/junit/tutorial/CalculatorParameterizedTest.java
@@ -56,67 +56,49 @@ public class CalculatorParameterizedTest {
 		}
 	}
 	
-	@RunWith(Theories.class)
-	public static class 除算メソッドのパラメータ化テスト {
-		@DataPoints
-		public static Fixture[] DATAS = {
-			new Fixture(3,  2, 1.5f),
-			new Fixture(10, 2, 5f),
-			new Fixture(3,  0, null),
-			new Fixture(10, 0, null),
-		};
-		@Rule
-		public ExpectedException exception = ExpectedException.none();
-		@Rule
-		public AssertionMessage message = new AssertionMessage();
-		@Theory
-		public void divideで除算結果を取得できること(Fixture fx) {
-			Assume.assumeTrue(fx.y != 0);
-			message.append("case: %d * %d = %d", fx.x, fx.y, fx.expected);
-			Calculator calc = new Calculator();
-			float expected = fx.expected;
-			float actual = calc.divide(fx.x, fx.y);
-			assertThat(actual, is(expected));
-		}
-		@Theory
-		public void divideで第2引数に0を設定した場合にはilleagalArgumentExceptionを送出する(Fixture fx) {
-			Assume.assumeTrue(fx.y == 0);
-			exception.expect(IllegalArgumentException.class);
-			Calculator calc = new Calculator();
-			calc.divide(5, 0);
-		}
-		
-		static class Fixture {
-			int x, y;
-			Float expected;
-			
-			Fixture(int x, int y, Float expected) {
-				this.x = x;
-				this.y = y;
-				this.expected = expected;
-			}
-		}
-		
-	}
-	
-	
-//	@Test
-//	public void divideで3と２の除算結果が取得できる() {
-//		Calculator calc = new Calculator();
-//		float expected = 1.5f;
-//		float actual = calc.divide(3, 2);
-//		assertThat(actual, is(expected));
-//	}
-	
-//
-//	@Test
-//	public void testMultiply() {
-//		fail("まだ実装されていません"); // TODO
-//	}
-//
-//	@Test
-//	public void testDivide() {
-//		fail("まだ実装されていません"); // TODO
-//	}
+    @RunWith(Theories.class)
+    public static class 除算メソッドのパラメータ化テスト {
+        @Rule
+        public AssertionMessage message = new AssertionMessage();
+        @DataPoints
+        public static Fixture[] DATAs = {
+                new Fixture(3, 2, 1.5f),
+                new Fixture(10, 2, 5f),
+                new Fixture(3, 0, null),
+                new Fixture(10, 0, null),
+        };
+        @Rule
+        public ExpectedException exception = ExpectedException.none();
+
+        @Theory
+        public void divideで除算結果が取得できること(Fixture fx) {
+            Assume.assumeTrue(fx.y != 0);
+            message.append("case: %d / %d = %f", fx.x, fx.y, fx.expected);
+            Calculator calc = new Calculator();
+            float expected = fx.expected;
+            float actual = calc.divide(fx.x, fx.y);
+            assertThat(actual, is(expected));
+        }
+
+        @Theory
+        public void divideの第２引数に0を指定した場合にはIllegalArgumentExceptionを送出する(Fixture fx) {
+            Assume.assumeTrue(fx.y == 0);
+            message.append("case: %d / %d > Exception", fx.x, fx.y);
+            exception.expect(IllegalArgumentException.class);
+            Calculator calc = new Calculator();
+            calc.divide(fx.x, fx.y);
+        }
+
+        static class Fixture {
+            int x, y;
+            Float expected;
+
+            Fixture(int x, int y, Float expected) {
+                this.x = x;
+                this.y = y;
+                this.expected = expected;
+            }
+        }
+    }
 
 }
