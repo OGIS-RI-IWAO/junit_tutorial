f3ab8f09a898bc1216adf053a6a437594e468d9b
iwao harada <harada_iwao@ogis-ri.co.jp>
2012-06-29 17:40:41 +0900

    MS932の文字化け対応

--- src/cucumber/porker/Pat.java
+++ src/cucumber/porker/Pat.java
@@ -30,7 +30,7 @@ public abstract class Pat {
         OnePair(int no) {
             this.no = no;
         }
-        // hashCode,equalsメソッドは省略
+        // hashCode,equals
         @Override
         public int hashCode() {
             final int prime = 31;
